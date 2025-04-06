7efffbb499e88ff2fe2496eebe1fae49e88a47af
Leonardo Martins Martins <leonardo.mmartins@outlook.com>
2025-04-06 14:08:53 -0300

    jpql em queries

--- src/main/java/br/sistema_recomendacoes/repository/GeneroRepository.java
+++ src/main/java/br/sistema_recomendacoes/repository/GeneroRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.repository.query.Param;
 import br.sistema_recomendacoes.model.Genero;
 
 public interface GeneroRepository extends JpaRepository<Genero, Long> {
-    @Query(nativeQuery = true, value = "select count(*) from livro_genero where genero_id= :genero_id;")
-    Integer countNum_livros(@Param("genero_id") Integer genero_id);
+    @Query("SELECT COUNT(l) FROM Livro l JOIN l.generos g WHERE g.id = :genero_id")
+    Integer countNum_livros(@Param("genero_id") Integer genero_id);    
 
     Optional<Genero> findByNome(String nome);
 }
